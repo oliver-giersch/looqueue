df5e0ecbfb6795e2fcc6be23f0db1b69ac9ae30b
oliver-giersch <oliver.giersch@b-tu.de>
2020-07-14 15:39:29 +0200

    updates build script

--- build_cluster.sh
+++ build_cluster.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-#module load llvm/9.0.0
-module load gcc/9.3.0
-module load boost/1.72.0
-module load intel/19.0.5
-module load cmake
+module load \
+  llvm/9.0.0 \
+  gcc/9.2.0 \
+  boost/1.72.0 \
+  intel/19.0.5 \
+  cmake || exit
 
 rm -r cmake-build-remote-release
 mkdir cmake-build-remote-release
@@ -17,7 +18,8 @@ cmake -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_C_COMPILER=icc \
       -DCMAKE_CXX_COMPILER=icpc \
       -DALLOCATOR=rpmalloc \
-      -G "CodeBlocks - Unix Makefiles" "$(dirname "$(pwd)")"
+      -G "CodeBlocks - Unix Makefiles" "$(dirname "$(pwd)")" || exit
 
 cd ..
 cmake --build cmake-build-remote-release/ --target bench_throughput -- -j 1
+module unload cmake gcc boost intel cmake
